afc0b3a54558a1199d532a60ce610f4cc4a552e7
Sona Ann Thomas <32.sona.antho@gmail.com>
2024-04-13 23:12:10 +0530

    welcome folder added

--- tk/views.py
+++ tk/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def singIn(request):
+
+    return render (request, "signin/signIn.html")
+
+
--- tripkerala/settings.py
+++ tripkerala/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tripkerala.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
